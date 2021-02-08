14fb27fd339503d42104e824d8cec18c644c6814
AoiSakai <aoi_sakai@icloud.com>
2021-02-08 14:29:59 +0900

    カウントアプリの完成

--- Count6/ViewController.swift
+++ Count6/ViewController.swift
@@ -8,10 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label:UILabel!
+    var number:Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        label.text = String(number)
+    }
+    
+    @IBAction func plus(){
+        number += 1
+        label.text = String(number)
+        color()
+    }
+    
+    @IBAction func minus(){
+        number -= 1
+        label.text = String(number)
+        color()
+    }
+    
+    @IBAction func clear(){
+        number = 0
+        label.text = String(number)
+        color()
+    }
+    
+    func color(){
+        if number % 15 == 0 {
+            label.textColor = UIColor.red
+        }else if number % 3 == 0{
+            label.textColor = UIColor.green
+        }else if number % 5 == 0{
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.black
+        }
     }
 
 
